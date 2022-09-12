2d328b879accf9347a5cd1deb1d938bcf5542679
Jason Kuhrt <jasonkuhrt@me.com>
2022-09-11 22:11:08 -0400

    refactor

--- packages/types/tests/index.spec.ts
+++ packages/types/tests/index.spec.ts
@@ -4,21 +4,10 @@ import { expectType } from 'tsd'
 // eslint-disable-next-line
 const as = <T>(): T => 0 as any
 
-// TestMixed
-//prettier-ignore
-namespace _ {
-	expectType<{ long: undefined; short: 'v'; aliases: { short: []; long: [] } }>(as<							FlagName.Parse<'-v'>>())
-	expectType<{ long: undefined; short: 'v'; aliases: { short: ['x']; long: [] } }>(as<					FlagName.Parse<'-v -x'>>())
-	expectType<{ long: undefined; short: 'v'; aliases: { short: ['x', 'y']; long: [] } }>(as<			FlagName.Parse<'-v -x -y'>>())
-	expectType<{ long: 'vv'; short: undefined; aliases: { short: []; long: ['xx'] } }>(as<				FlagName.Parse<'--vv --xx'>>())
-	expectType<{ long: 'vv'; short: undefined; aliases: { short: []; long: ['xx', 'yy'] } }>(as<	FlagName.Parse<'--vv --xx --yy'>>())
-	expectType<{ long: 'vv'; short: 'v'; aliases: { short: ['x']; long: ['xx'] } }>(as<						FlagName.Parse<'-v --vv -x --xx'>>())
-	expectType<{ long: 'vv'; short: 'v'; aliases: { short: ['x']; long: ['xx'] } }>(as<						FlagName.Parse<'v vv x xx'>>())
-	expectType<{ long: 'vv'; short: 'v'; aliases: { short: ['x']; long: ['xx'] } }>(as<						FlagName.Parse<'v --vv x xx'>>())
-}
+expectType<{ long: 'filePath' }>(as<FlagName.Parse<'--file-path'>>())
 
 // prettier-ignore
-namespace _TestErrors {
+namespace _testErrors {
 	expectType<FlagName.Errors.Empty>(as<											FlagName.Parse<''>>())
 	expectType<FlagName.Errors.Empty>(as<											FlagName.Parse<' '>>())
 	expectType<FlagName.Errors.NameReserved<'abc'>>(as<				FlagName.Parse<'--abc', { reservedNames: 'abc'; usedNames: undefined }>>())
@@ -30,6 +19,18 @@ namespace _TestErrors {
 	expectType<FlagName.Errors.ShortFlagTooLong<'vv'>>(as<		FlagName.Parse<'-vv'>>())
 	expectType<FlagName.Errors.AliasDuplicate<'vv'>>(as<			FlagName.Parse<'--vv --vv'>>())
 	expectType<FlagName.Errors.AliasDuplicate<'v'>>(as<				FlagName.Parse<'-v -v'>>())
+}
+
+//prettier-ignore
+namespace _mixed {
+	expectType<{ long: undefined; short: 'v'; aliases: { short: []; long: [] } }>(as<							FlagName.Parse<'-v'>>())
+	expectType<{ long: undefined; short: 'v'; aliases: { short: ['x']; long: [] } }>(as<					FlagName.Parse<'-v -x'>>())
+	expectType<{ long: undefined; short: 'v'; aliases: { short: ['x', 'y']; long: [] } }>(as<			FlagName.Parse<'-v -x -y'>>())
+	expectType<{ long: 'vv'; short: undefined; aliases: { short: []; long: ['xx'] } }>(as<				FlagName.Parse<'--vv --xx'>>())
+	expectType<{ long: 'vv'; short: undefined; aliases: { short: []; long: ['xx', 'yy'] } }>(as<	FlagName.Parse<'--vv --xx --yy'>>())
+	expectType<{ long: 'vv'; short: 'v'; aliases: { short: ['x']; long: ['xx'] } }>(as<						FlagName.Parse<'-v --vv -x --xx'>>())
+	expectType<{ long: 'vv'; short: 'v'; aliases: { short: ['x']; long: ['xx'] } }>(as<						FlagName.Parse<'v vv x xx'>>())
+	expectType<{ long: 'vv'; short: 'v'; aliases: { short: ['x']; long: ['xx'] } }>(as<						FlagName.Parse<'v --vv x xx'>>())
 }
 
 interface SomeLong {
@@ -62,7 +63,7 @@ expectType<SomeShort>(as<FlagName.Parse<'-v '>>())
 expectType<SomeShort>(as<FlagName.Parse<'v '>>())
 expectType<SomeShort>(as<FlagName.Parse<'v'>>())
 
-type SomeLongShort = {
+interface SomeLongShort {
   long: 'version'
   short: 'v'
   aliases: { short: []; long: [] }
@@ -82,5 +83,3 @@ expectType<SomeLongShort>(as<FlagName.Parse<'  -v  --version  '>>())
 expectType<SomeLongShort>(as<FlagName.Parse<'  -v   --version '>>())
 expectType<SomeLongShort>(as<FlagName.Parse<'  v   version '>>())
 expectType<SomeLongShort>(as<FlagName.Parse<'v version'>>())
-
-expectType<{ long: 'filePath' }>(as<FlagName.Parse<'--file-path'>>())
