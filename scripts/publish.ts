28a618feea3f611ff6147d250bb9c8d7c0794928
Jason Kuhrt <jasonkuhrt@me.com>
2022-09-14 21:11:17 -0400

    fix: git add before commit

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -12,9 +12,11 @@ const args = Command.create({
 }).parseOrThrow()
 
 const cwd = Path.join(Path.dirname(url.fileURLToPath(import.meta.url)), `../packages`, args.package)
+const $Fs = Fs.cwd(cwd)
 
-const pkg = (await Fs.readAsync(Path.join(cwd, `package.json`), `json`)) as { name: string; version: string }
-await Fs.writeAsync(Path.join(cwd, `package.json`), { ...pkg, version: args.version }, { jsonIndent: 2 })
+const pkg = (await $Fs.readAsync(`package.json`, `json`)) as { name: string; version: string }
+await $Fs.writeAsync(`package.json`, { ...pkg, version: args.version }, { jsonIndent: 2 })
+await execa(`git`, [`add`, `package.json`], { cwd })
 await execa(`git`, [`commit`, `--message`, `'chore(${args.package}): bump version'`], { stdio: `inherit` })
 await execa(`pnpm`, [`publish`, `--access public`], { cwd, stdio: `inherit` })
 await execa(`git`, [`tag`, args.version], { stdio: `inherit` })
