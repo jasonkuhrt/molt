82e1580bee59ddebf5a88f37dd25351a1caef630
Jason Kuhrt <jasonkuhrt@me.com>
2022-09-15 22:23:22 -0400

    chore: allow publishing by version bump

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -4,12 +4,14 @@ import { execa } from 'execa'
 import Fs from 'fs-jetpack'
 import * as Path from 'node:path'
 import url from 'node:url'
+import * as Semver from 'semver'
 import semverRegex from 'semver-regex'
 import { z } from 'zod'
 
 const args = Command.create({
   'p package': z.enum([`@molt/command`, `@molt/types`, `molt`]),
-  'v version': z.string().regex(semverRegex()),
+  'v version': z.string().regex(semverRegex()).optional(),
+  'b bump': z.enum([`major`, `minor`, `patch`]).optional(),
   publish: z.boolean().default(true),
   githubRelease: z.boolean().default(true),
 }).parseOrThrow()
@@ -17,6 +19,8 @@ const args = Command.create({
 const githubToken = process.env[`GITHUB_TOKEN`]
 
 if (!githubToken) throw new Error(`GITHUB_TOKEN is required`)
+
+if (!args.bump && args.version) throw new Error(`--bump or --version is required`)
 
 const cwd = Path.join(Path.dirname(url.fileURLToPath(import.meta.url)), `../packages`, args.package)
 const $Fs = Fs.cwd(cwd)
@@ -33,7 +37,9 @@ const pkg = (await $Fs.readAsync(`package.json`, `json`)) as {
   repository: string
 }
 
-const gitTagName = `${args.package}@${args.version}`
+//eslint-disable-next-line
+const newVersion = args.bump ? Semver.inc(pkg.version, args.bump)! : args.version!
+const gitTagName = `${args.package}@${newVersion}`
 
 const match = workspacePkg.repository.match(/git@github.com:(.+)\/(.+)\.git/)
 
@@ -47,19 +53,12 @@ const repo = {
 }
 
 if (args.publish) {
-  await $Fs.writeAsync(
-    `package.json`,
-    JSON.stringify({ ...pkg, version: args.version }, null, 2) + `\n, repository: string`
-  )
+  await $Fs.writeAsync(`package.json`, JSON.stringify({ ...pkg, version: newVersion }, null, 2) + `\n`)
   await execa(`git`, [`add`, `package.json`], { cwd })
   await execa(`git`, [`commit`, `--message`, `'chore(${args.package}): bump version'`], { stdio: `inherit` })
   await execa(`pnpm`, [`publish`], { cwd, stdio: `inherit` })
   // prettier-ignore
-  await execa(
-  `git`,
-  [`tag`, gitTagName, `--annotate`, `--message`, `Version ${args.version} for package ${args.package}`],
-  { stdio: `inherit` }
-)
+  await execa(`git`, [`tag`, gitTagName, `--annotate`, `--message`, `Version ${newVersion} for package ${args.package}`], { stdio: `inherit` })
   await execa(`git`, [`push`], { stdio: `inherit` })
   await execa(`git`, [`push`, `--tags`], { stdio: `inherit` })
 }
