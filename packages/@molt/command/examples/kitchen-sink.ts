d7b6211ff106a8d1946e0995fdc977fe5dbeddc8
Jason Kuhrt <jasonkuhrt@me.com>
2022-11-04 20:05:53 -0400

    docs

--- packages/@molt/command/examples/kitchen-sink.ts
+++ packages/@molt/command/examples/kitchen-sink.ts
@@ -2,12 +2,12 @@ import { Command } from '../src/index.js'
 import { z } from 'zod'
 
 const args = Command.parameters({
-  '--bad-default': z.string().default(() => {
+  badDefault: z.string().default(() => {
     throw new Error(`whoops`)
   }),
-  '--one': z.enum([`apple`]),
-  '--figbar': z.enum([`zebra`, `monkey`, `giraffe`]).default(`monkey`),
-  '--big': z
+  one: z.enum([`apple`]),
+  figbar: z.enum([`zebra`, `monkey`, `giraffe`]).default(`monkey`),
+  big: z
     .enum([
       `apple`,
       `baby`,
@@ -22,15 +22,28 @@ const args = Command.parameters({
       `kilomanjara`,
     ])
     .optional(),
-  '--file-path': z.string().describe(`Path to the file to convert.`),
-  '--to': z.enum([`json`, `yaml`, `toml`]).describe(`Format to convert to.`),
-  '--from': z
+  filePath: z.string().describe(`Path to the file to convert.`),
+  to: z.enum([`json`, `yaml`, `toml`]).describe(`Format to convert to.`),
+  from: z
     .enum([`json`, `yaml`, `toml`])
     .optional()
     .describe(`Format to convert from. By default inferred from the file extension.`),
-  '--verbose -v': z.boolean().default(false).describe(`Log detailed progress as conversion executes.`),
-  '--move -m': z.boolean().default(false).describe(`Delete the original file after it has been converted.`),
+  'verbose v': z.boolean().default(false).describe(`Log detailed progress as conversion executes.`),
+  'move m': z.boolean().default(false).describe(`Delete the original file after it has been converted.`),
 })
+  .parametersExclusive(`desert`, (_) =>
+    _.parameter(
+      `cake`,
+      z
+        .enum([`chocolate`, `vanilla`, `strawberry`])
+        .describe(`An indulgent treat to celebrate peak moments in life!`)
+    ).parameter(
+      `fruit`,
+      z
+        .enum([`apple`, `banana`, `orange`])
+        .describe(`A sustainable snack for everyday happiness and delight!`)
+    )
+  )
   .settings({
     parameters: {
       environment: {
