5499611dee04e64f8612466d2b05b9ff10f71a36
Jason Kuhrt <jasonkuhrt@me.com>
2022-11-04 20:39:48 -0400

    chore(@molt/command): update docs intro and toc

--- packages/@molt/command/examples/intro.ts
+++ packages/@molt/command/examples/intro.ts
@@ -1,16 +1,32 @@
 import { Command } from '../src/index.js'
 import { z } from 'zod'
 
-const args = Command.parameters({
-  '--file-path': z.string().describe(`Path to the file to convert.`),
-  '--to': z.enum([`json`, `yaml`, `toml`]).describe(`Format to convert to.`),
-  '--from': z
-    .enum([`json`, `yaml`, `toml`])
-    .optional()
-    .describe(`Format to convert from. By default inferred from the file extension.`),
-  '--verbose -v': z.boolean().default(false).describe(`Log detailed progress as conversion executes.`),
-  '--move -m': z.boolean().default(false).describe(`Delete the original file after it has been converted.`),
-}).parse()
+// prettier-ignore
+const args = Command
+  .parameter(`filePath`, z
+    .string()
+    .describe(`Path to the file to convert.`)
+  )
+  .parameter(`to`, z
+     .enum([`json`, `yaml`, `toml`])
+     .describe(`Format to convert to.`)
+  )
+  .parameter(`from`, z
+     .enum([`json`, `yaml`, `toml`])
+     .optional()
+     .describe(`Format to convert from. By default inferred from the file extension.`)
+  )
+  .parameter(`verbose v`, z
+     .boolean()
+     .default(false)
+     .describe(`Log detailed progress as conversion executes.`)
+  )
+  .parameter(`move m`, z
+    .boolean()
+    .default(false)
+    .describe(`Delete the original file after it has been converted.`)
+  )
+  .parse()
 
 args.filePath
 args.from
