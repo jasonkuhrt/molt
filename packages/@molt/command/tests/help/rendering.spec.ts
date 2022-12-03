67775391d53c14b513e85463ee0a473929ea31d8
Jason Kuhrt <jasonkuhrt@me.com>
2022-12-03 12:55:11 -0500

    fix: account for type col header length

--- packages/@molt/command/tests/help/rendering.spec.ts
+++ packages/@molt/command/tests/help/rendering.spec.ts
@@ -15,6 +15,13 @@ it(`if there is optional param it is shown`, () => {
   expect(output).toMatchSnapshot(`polychrome`)
 })
 
+it(`if there are one or more parameter descriptions then call the "type" col header "type/description"`, () => {
+  Command.parameters({ a: s.optional().describe(`Blah blah blah blah.`) }).parse({ line: [`-h`] })
+  const output = processStdout.mock.lastCall?.[0] as string
+  expect(stripAnsi(output)).toMatchSnapshot(`monochrome`)
+  expect(output).toMatchSnapshot(`polychrome`)
+})
+
 it(`if parameter has description it is shown`, () => {
   Command.parameters({ a: s.optional().describe(`Blah blah blah.`) }).parse({ line: [`-h`] })
   const output = processStdout.mock.lastCall?.[0] as string
