b15103a5dc3df074ea993708a6b0b3afaddf1065
Jason Kuhrt <jasonkuhrt@me.com>
2023-03-10 10:42:43 +0000

    tests

--- packages/@molt/command/tests/prompt/prompt.spec.ts
+++ packages/@molt/command/tests/prompt/prompt.spec.ts
@@ -1,18 +1,27 @@
 import { Command } from '../../src/index.js'
-import { s } from '../_/helpers.js'
+import { s, tryCatch } from '../_/helpers.js'
 import { tty } from '../_/mocks/tty.js'
 import { expect, it } from 'vitest'
 
 it.each(
   // prettier-ignore
   [
-    [`a required parameter can be given at prompt`, { a: { schema: s, prompt: true } }, [], [`foo`]],
+    [`a required parameter can be given at prompt`,                   { a: { schema: s, prompt: true } },             [], [`foo`]],
+    [`a required parameter given by flag is not asked for in prompt`, { a: { schema: s, prompt: true } },             [`-a`,`foo`], []],
+    [`a required parameter opted out of prompt is not prompted for`,  { a: { schema: s, prompt: false } },            [], []],
+    [`an optional parameter can be skipped at prompt`,                { a: { schema: s.optional(), prompt: true } },  [], []],
+    [`by default parameter prompt is disabled`,                       { a: { schema: s } },                           [], []],
+    //todo
+    [`prompt input is validated`,                                     { a: { schema: s.min(2), prompt:true } },       [], [`x`]],
   ] satisfies [string, any, string[], string[]][]
 )(`%s`, (_, parameters, line, ttyReadsScript) => {
   tty.script.reads(ttyReadsScript)
-  const args = Command.parameters(parameters)
-    .settings({ onError: `throw` })
-    .parse({ line, tty: tty.interface })
-  expect(args).toMatchSnapshot()
-  expect(tty.state.history.full).toMatchSnapshot()
+  const args = tryCatch(() =>
+    Command.parameters(parameters)
+      .settings({ onError: `throw`, helpOnError: false })
+      .parse({ line, tty: tty.interface })
+  )
+
+  expect(args).toMatchSnapshot(`args`)
+  expect(tty.state.history.full).toMatchSnapshot(`tty`)
 })
