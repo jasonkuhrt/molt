1d46cf168454e281fcd198cff901a9648ea32a5d
Jason Kuhrt <jasonkuhrt@me.com>
2023-09-07 22:42:34 -0400

    clean

--- packages/@molt/command/tests/prompt/prompt.spec.ts
+++ packages/@molt/command/tests/prompt/prompt.spec.ts
@@ -8,7 +8,7 @@ import stripAnsi from 'strip-ansi'
 import { describe, expect, it } from 'vitest'
 
 let parameters: Methods.Parameters.InputAsConfig<Schema>
-let ttyMockDataInputs: string[]
+let ttyInputs: string[]
 let line: string[]
 const settings: Settings.Input = {}
 
@@ -17,7 +17,7 @@ describe(`prompt can be configured at the parameter level`, () => {
     parameters = {
       a: { schema: s.min(2), prompt: { when: { result: `rejected`, error: `ErrorMissingArgument` } } },
     }
-    ttyMockDataInputs = [`foo`]
+    ttyInputs = [`foo`]
     line = []
     run()
   })
@@ -26,7 +26,7 @@ describe(`prompt can be configured at the parameter level`, () => {
     parameters = {
       a: { schema: s.min(2), prompt: { when: { result: `rejected`, error: `ErrorInvalidArgument` } } },
     }
-    ttyMockDataInputs = [`foo`]
+    ttyInputs = [`foo`]
     line = [`-a`, `1`]
     run()
   })
@@ -38,7 +38,7 @@ describe(`prompt can be configured at the parameter level`, () => {
         prompt: { when: { result: `rejected`, error: [`ErrorInvalidArgument`, `ErrorMissingArgument`] } },
       },
     }
-    ttyMockDataInputs = [`foo`]
+    ttyInputs = [`foo`]
     line = [`-a`, `1`]
     run()
   })
@@ -50,10 +50,11 @@ describe(`prompt can be configured at the parameter level`, () => {
         prompt: { when: { result: `omitted`, spec: { optionality: [`default`, `optional`] } } },
       },
     })
-    ttyMockDataInputs = [`foo`]
+    ttyInputs = [`foo`]
     line = []
     run()
   })
+
   it(`static error to match on omitted event on required parameter`, () => {
     // @ts-expect-error not available
     Command.parameters({
@@ -81,20 +82,8 @@ describe(`prompt can be configured at the parameter level`, () => {
   })
 })
 
-// todo: need to implement the ParseResult type in code
-
-// describe(`the default prompt can be configured at the command level`, () => {
-//   it(``, () => {
-//     parameters = { a: { schema: s.min(2) } }
-//     settings = { prompt: { patterns: { $name: 'x', rejected: { name: [`ErrorInvalidArgument`, `ErrorMissingArgument`] } }} }
-//     ttyReadsScript = [`foo`]
-//     line = []
-//     run()
-//   })
-// })
-
 const run = () => {
-  tty.mock.input.add(ttyMockDataInputs)
+  tty.mock.input.add(ttyInputs)
   const args = tryCatch(() =>
     Command.parameters(parameters)
       .settings({ onError: `throw`, helpOnError: false, ...settings })
@@ -122,41 +111,3 @@ describe(`types`, () => {
     }
   })
 })
-
-// it.each(
-//   // prettier-ignore
-//   [
-//     [`a required parameter can be given at prompt`,                                               { a: { schema: s, prompt: true } },             [], [`foo`]],
-//     [`a required parameter given by flag is not asked for in prompt`,                             { a: { schema: s, prompt: true } },             [`-a`,`foo`], []],
-//     [`a required parameter opted out of prompt is not prompted for`,                              { a: { schema: s, prompt: false } },            [], []],
-//     [`an optional parameter can be skipped at prompt`,                                            { a: { schema: s.optional(), prompt: true } },  [], []],
-//     [`by default parameter prompt is disabled`,                                                   { a: { schema: s } },                           [], []],
-//     [`prompt input is validated`,                                                                 { a: { schema: s.min(2), prompt:true } },       [], [`x`,`xx`]],
-//     // todo we may want to change this behavior
-//     [`a parameter with prompt enabled given an invalid value at line does not prompt`,            { a: { schema: s.min(2), prompt: true } },      [`-a`,`x`], []],
-//     [`shows progress gutter even when there is only one thing to prompt`,                         { a: { schema: s, prompt: true } },             [], [`foo`]],
-//   ] satisfies [description: string, config:any, line:string[], tty:string[]][]
-// )(`%s`, (_, parameters, line, ttyReadsScript) => {
-//   tty.script.userInputs(ttyReadsScript)
-//   const args = tryCatch(() =>
-//     Command.parameters(parameters)
-//       .settings({ onError: `throw`, helpOnError: false })
-//       .parse({ line, tty: tty.interface })
-//   )
-//   expect(args).toMatchSnapshot(`args`)
-//   expect(tty.state.history.full).toMatchSnapshot(`tty`)
-//   expect(tty.state.history.full.map((_) => stripAnsi(_))).toMatchSnapshot(`tty strip ansi`)
-// })
-
-// it(`when there is no tty then prompt is skipped`, () => {
-//   // When running in Vitest it is already false
-//   // process.stdout.isTTY = false
-//   const args = tryCatch(() =>
-//     Command.parameters({ a: { schema: s, prompt: true } })
-//       .settings({ onError: `throw`, helpOnError: false })
-//       .parse({ line: [] })
-//   )
-//   expect(args).toMatchSnapshot(`args`)
-//   expect(tty.state.history.full).toMatchSnapshot(`tty`)
-//   expect(tty.state.history.full.map((_) => stripAnsi(_))).toMatchSnapshot(`tty strip ansi`)
-// })
