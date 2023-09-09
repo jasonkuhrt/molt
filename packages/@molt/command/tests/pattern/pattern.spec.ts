2aefb5b19445d55e94b289b5b9ffa3a198439c89
Jason Kuhrt <jasonkuhrt@me.com>
2023-09-09 11:46:37 -0400

    do not run static type tests

--- packages/@molt/command/tests/pattern/pattern.spec.ts
+++ packages/@molt/command/tests/pattern/pattern.spec.ts
@@ -37,74 +37,69 @@ it.each([
   [{ x: [`1`] }, { x: [[`1`], ['2']] }, true],
   [{ x: [`1`] }, { x: [`2`] }, false],
   [{ x: [`1`] }, { x: [`1`, '2'] }, false],
-  // it(`match array`, () => {
-  //   type D = { x: string[] }
-  //   const d: D = expect(match(d, { x: [`1`] })).toBe(true)
-  //   expect(match(d, { x: [[`1`], [`2`]] })).toBe(true)
-  //   expect(match(d, { x: [`2`] })).toBe(false)
-  //   expect(match(d, { x: [`1`, `2`] })).toBe(false)
-  // }),
 ])(`%s %s %s`, (data, pattern, expected) => {
   expect(match(data, pattern as any)).toBe(expected)
 })
 
-// array data
-const d = { x: [`1`] }
-// OK
-match(d, { x: [`1`] })
-match(d, { x: [[`1`], [`2`]] })
-match(d, { x: [`2`] })
-match(d, { x: [`1`, `2`] })
-// @ts-expect-error
-match(d, { x: `1` })
-// @ts-expect-error
-match(d, { x: [['1'], '2'] })
+const staticTypeTests_ = () => {
+  // array data
+  const d = { x: [`1`] }
+  // OK
+  match(d, { x: [`1`] })
+  match(d, { x: [[`1`], [`2`]] })
+  match(d, { x: [`2`] })
+  match(d, { x: [`1`, `2`] })
+  // @ts-expect-error
+  match(d, { x: `1` })
+  // @ts-expect-error
+  match(d, { x: [['1'], '2'] })
 
-// scalar
-// @ts-expect-error
-match(1, '1')
-match(1, 1)
-// @ts-expect-error
-match('a', 'b')
-match('a', 'a')
-// @ts-expect-error
-match(true, false)
-match(true, true)
+  // scalar
+  // @ts-expect-error
+  match(1, '1')
+  match(1, 1)
+  // @ts-expect-error
+  match('a', 'b')
+  match('a', 'a')
+  // @ts-expect-error
+  match(true, false)
+  match(true, true)
 
-// scalar non-deterministic
-match(1, [])
-match(1, [1])
-match(1, [1, 1])
-// // @ts-expect-error
-// checkMatches(null, [null])
-// // @ts-expect-error
-// checkMatches(true, [true])
-// @ts-expect-error
-match('a', ['a', 'b'])
-match('a', ['a', 'a'])
+  // scalar non-deterministic
+  match(1, [])
+  match(1, [1])
+  match(1, [1, 1])
+  // // @ts-expect-error
+  // checkMatches(null, [null])
+  // // @ts-expect-error
+  // checkMatches(true, [true])
+  // @ts-expect-error
+  match('a', ['a', 'b'])
+  match('a', ['a', 'a'])
 
-// shallow object
-// @ts-expect-error
-match({ a: 1 }, { a: `1` })
-match({ a: 1 }, { a: 1 })
-// shallow object non-deterministic
-// @ts-expect-error
-match({ a: 1 }, [{ a: '1' }])
-match({ a: 1 }, [{ a: 1 }, { a: 2 }])
-match({ a: 1 }, { a: [1, 2] })
+  // shallow object
+  // @ts-expect-error
+  match({ a: 1 }, { a: `1` })
+  match({ a: 1 }, { a: 1 })
+  // shallow object non-deterministic
+  // @ts-expect-error
+  match({ a: 1 }, [{ a: '1' }])
+  match({ a: 1 }, [{ a: 1 }, { a: 2 }])
+  match({ a: 1 }, { a: [1, 2] })
 
-// deep object
-// @ts-expect-error
-match({ a: { b1: 2, b2: 3 } }, { a: { b1: 'a' } })
-match({ a: { b1: 2, b2: 3 } }, { a: { b1: 2 } })
-match({ a: { b1: 2, b2: 3 } }, { a: {} })
-// deep object non-deterministic
-// @ts-expect-error
-match({ a: { b1: 2, b2: 3 } }, { a: [{ b1: 2 }, { b2: '3' }] })
-match({ a: { b1: 2, b2: 3 } }, { a: [{ b1: 2 }, { b2: 3 }] })
-match({ a: { b1: 2, b2: 3 } }, [{ a: { b1: 2 } }, {}])
-// todo this is a silly pattern and should be rejected
-match({ a: { b1: 2, b2: 3 } }, { a: [{}, {}] })
+  // deep object
+  // @ts-expect-error
+  match({ a: { b1: 2, b2: 3 } }, { a: { b1: 'a' } })
+  match({ a: { b1: 2, b2: 3 } }, { a: { b1: 2 } })
+  match({ a: { b1: 2, b2: 3 } }, { a: {} })
+  // deep object non-deterministic
+  // @ts-expect-error
+  match({ a: { b1: 2, b2: 3 } }, { a: [{ b1: 2 }, { b2: '3' }] })
+  match({ a: { b1: 2, b2: 3 } }, { a: [{ b1: 2 }, { b2: 3 }] })
+  match({ a: { b1: 2, b2: 3 } }, [{ a: { b1: 2 } }, {}])
+  // todo this is a silly pattern and should be rejected
+  match({ a: { b1: 2, b2: 3 } }, { a: [{}, {}] })
 
-// with unions
-match({ a: 1 as 1 | 2 }, { a: [1, 2] })
+  // with unions
+  match({ a: 1 as 1 | 2 }, { a: [1, 2] })
+}
