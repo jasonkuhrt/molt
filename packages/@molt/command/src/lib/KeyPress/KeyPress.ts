4f025d5665438e325349080c9da0963f0b9caf61
Jason Kuhrt <jasonkuhrt@me.com>
2023-09-23 21:40:46 -0400

    refactor(@molt/command): extract a render engine lib for prompt (#206)

--- packages/@molt/command/src/lib/KeyPress/KeyPress.ts
+++ packages/@molt/command/src/lib/KeyPress/KeyPress.ts
@@ -1,7 +1,41 @@
 import { stdin, stdout } from 'node:process'
 import * as Readline from 'node:readline'
 
-export interface KeyPressEvent<Name extends string = string> {
+export type Key =
+  | 'up'
+  | 'left'
+  | 'down'
+  | 'right'
+  | 'tab'
+  | 'return'
+  | 'a'
+  | 'b'
+  | 'c'
+  | 'd'
+  | 'e'
+  | 'f'
+  | 'g'
+  | 'h'
+  | 'i'
+  | 'j'
+  | 'k'
+  | 'l'
+  | 'm'
+  | 'n'
+  | 'o'
+  | 'p'
+  | 'q'
+  | 'r'
+  | 's'
+  | 't'
+  | 'u'
+  | 'v'
+  | 'w'
+  | 'x'
+  | 'y'
+  | 'z'
+
+export interface KeyPressEvent<Name extends Key = Key> {
   name: Name
   ctrl: boolean
   meta: boolean
