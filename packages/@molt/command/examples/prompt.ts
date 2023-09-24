3f58dbe56251039281202e566e3bf3f4d1c296b3
Jason Kuhrt <jasonkuhrt@me.com>
2023-09-24 09:57:12 -0400

    fix(@molt/command): prompt optional numbers and strings (#209)

--- packages/@molt/command/examples/prompt.ts
+++ packages/@molt/command/examples/prompt.ts
@@ -2,12 +2,20 @@ import { Command } from '../src/index.js'
 import { z } from 'zod'
 
 const args = await Command.create()
+  // required
   .parameter(`alpha`, z.string())
   .parameter(`bravo`, z.number())
   .parameter(`charlie`, z.boolean())
   .parameter(`delta`, z.enum([`echo`, `foxtrot`, `golf`]))
-  .parameter(`honda`, z.union([z.string(), z.number(), z.enum([`a`, `b`, `c`])]))
-  .settings({ prompt: true })
+  .parameter(`hotel`, z.union([z.string(), z.number(), z.enum([`a`, `b`, `c`])]))
+  // optional
+  .parameter(`india`, z.string().optional())
+  .parameter(`juliet`, z.number().optional())
+  .parameter(`kilo`, z.boolean().optional())
+  .parameter(`lima`, z.enum([`a`, `b`, `c`]).optional())
+  .parameter(`mike`, z.union([z.string(), z.number(), z.enum([`a`, `b`, `c`])]).optional())
+  // end
+  .settings({ prompt: { when: [{ result: `rejected` }, { result: `omitted` }] } })
   .parse()
 
 console.log()
