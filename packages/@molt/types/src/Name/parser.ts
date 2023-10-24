b43324455201d9f7b59a089ca1b1e13c754730a7
Jason Kuhrt <jasonkuhrt@me.com>
2023-10-23 22:10:27 -0400

    refactor(@molt/types): use hotscript (#238)

--- packages/@molt/types/src/Name/parser.ts
+++ packages/@molt/types/src/Name/parser.ts
@@ -1,6 +1,7 @@
-import type { Strings } from '../prelude.js'
+// import type { Strings } from '../prelude.js'
 import type { BaseChecks, LongChecks, ReportFailures, ShortChecks, SomeFailures } from './checks.js'
 import type { Name, NameEmpty } from './data.js'
+import type { $, Objects, Strings, Tuples } from 'hotscript'
 
 // prettier-ignore
 export namespace Errors {
@@ -10,20 +11,42 @@ export namespace Errors {
 }
 
 // prettier-ignore
-type AddAliasLong<$Name extends Name, Variant extends string> = Omit<$Name, 'aliases'> & { aliases: { long: [...$Name['aliases']['long'], Strings.KebabToCamelCase<Variant>], short: $Name['aliases']['short'] }}
+type Add<
+	Kind extends 'short'|'long',
+	$Name extends Name,
+	Variant extends string
+> =
+	Kind extends 'short' ?
+		$Name['short'] extends null 	? AddShort<$Name,Variant> :
+																		AddAliasShort<$Name,Variant> :
+	Kind extends 'long' ?
+		$Name['long'] extends null 	? AddLong<$Name,Variant> :
+																 	AddAliasLong<$Name,Variant> :
+	never
+
 // prettier-ignore
-type AddAliasShort<$Name extends Name, Variant extends string> = Omit<$Name, 'aliases'> & { aliases: { long: $Name['aliases']['long'], short: [...$Name['aliases']['short'], Variant] }}
+type AddAliasLong<$Name extends Name, Variant extends string> =
+	$<Objects.Update<'aliases.long', Tuples.Append<$<Strings.CamelCase,Variant>>>, $Name>
+
 // prettier-ignore
-type AddLong<$Name extends Name, Variant extends string> = Omit<$Name, 'long'> & { long: Strings.KebabToCamelCase<Variant>  }
+type AddAliasShort<$Name extends Name, Variant extends string> =
+	$<Objects.Update<'aliases.short', Tuples.Append<Variant>>, $Name>
+
 // prettier-ignore
-type AddShort<$Name extends Name, Variant extends string> = Omit<$Name, 'short'> & { short: Variant  }
+type AddLong<$Name extends Name, Variant extends string> =
+	$<Objects.Update<'long', $<Strings.CamelCase,Variant>>, $Name>
+
+// prettier-ignore
+type AddShort<$Name extends Name, Variant extends string> =
+	$<Objects.Update<'short', Variant>, $Name>
+
 // prettier-ignore
 type addCanonical<$Name extends Name> =
-	Omit<$Name, 'canonical'> & {
-		canonical:	$Name['long']  extends string ? $Name['long'] :
-								$Name['short'] extends string ? $Name['short'] :
-																								never // A valid flag always has either a long or short name
-	}
+	$<Objects.Update<'canonical', 
+			$Name['long']  extends string ? $Name['long'] :
+			$Name['short'] extends string ? $Name['short'] :
+																			never // A valid flag always has either a long or short name
+	>, $Name>
 
 export interface SomeLimits {
   reservedNames: string | undefined
@@ -51,45 +74,73 @@ type _Parse<E extends string, Limits extends SomeLimits, $Name extends Name> =
 	E extends `${infer initial} `                        	? _Parse<initial, Limits, $Name> :
 
 	// Capture a long flag & continue
-	E extends `--${infer v} ${infer tail}`            	? LongChecks<v, Limits, $Name> extends SomeFailures ? ReportFailures<LongChecks<v, Limits, $Name>> :
-																												 	$Name['long'] extends null ?
-																												 		_Parse<tail, Limits, AddLong<$Name, v>> :
-																												 	 	_Parse<tail, Limits, AddAliasLong<$Name, v>> :
+	E extends `--${infer v} ${infer tail}`            	? LongChecks<v, Limits, $Name> extends SomeFailures ?
+																													ReportFailures<LongChecks<v, Limits, $Name>> :
+																													_Parse<tail, Limits,
+																														Add<
+																															'long',
+																															$Name,
+																															v
+																													>
+																												>
+																										:
 	// Capture a long name & Done!
-	E extends `--${infer v}` 							          	? LongChecks<v, Limits, $Name> extends SomeFailures ? ReportFailures<LongChecks<v, Limits, $Name>> :
-																														$Name['long'] extends null ?
-																														_Parse<'', Limits, AddLong<$Name, v>> :
-																														_Parse<'', Limits, AddAliasLong<$Name, v>> :
+	E extends `--${infer v}` 							          	? LongChecks<v, Limits, $Name> extends SomeFailures ?
+																												ReportFailures<LongChecks<v, Limits, $Name>> :
+																												_Parse<'', Limits,
+																													Add<
+																														'long',
+																														$Name,
+																														v
+																													>
+																												>
+																										:
 
 	// Capture a short flag & continue
-	E extends `-${infer v} ${infer tail}`            	? ShortChecks<v, Limits, $Name> extends SomeFailures ? ReportFailures<ShortChecks<v, Limits, $Name>> :
-																														$Name['short'] extends null ?
-																															_Parse<tail, Limits, AddShort<$Name, v>> :
-																															_Parse<tail, Limits, AddAliasShort<$Name, v>> :
+	E extends `-${infer v} ${infer tail}`            	? ShortChecks<v, Limits, $Name> extends SomeFailures ?
+																												ReportFailures<ShortChecks<v, Limits, $Name>> :
+																												_Parse<tail, Limits,
+																													Add<
+																														'short',
+																														$Name,
+																														v
+																													>
+																												>
+																											:
 	// Capture a short name & Done!
-	E extends `-${infer v}` 							            	? ShortChecks<v, Limits, $Name> extends SomeFailures ? ReportFailures<ShortChecks<v, Limits, $Name>> :
-																														$Name['short'] extends null ?
-																														_Parse<'', Limits, AddShort<$Name, v>> :
-																														_Parse<'', Limits, AddAliasShort<$Name, v>> :
+	E extends `-${infer v}` 							            	? ShortChecks<v, Limits, $Name> extends SomeFailures ?
+																													ReportFailures<ShortChecks<v, Limits, $Name>> :
+																													_Parse<'', Limits,
+																														Add<
+																															'short',
+																															$Name,
+																															v
+																														>
+																													>
+																											:
 
 	// Capture a long flag & continue
-	E extends `${infer v} ${infer tail}`             	? BaseChecks<v, Limits, $Name> extends SomeFailures ? ReportFailures<BaseChecks<v, Limits, $Name>> :
-																														Strings.Length<v> extends 1 ?
-																															$Name['short'] extends null ?
-																																_Parse<tail, Limits, AddShort<$Name, v>> :
-																																_Parse<tail, Limits, AddAliasShort<$Name, v>> :
-																															$Name['long'] extends null ?
-																																_Parse<tail, Limits, AddLong<$Name, v>> :
-																																_Parse<tail, Limits, AddAliasLong<$Name, v>> :
+	E extends `${infer v} ${infer tail}`             	? BaseChecks<v, Limits, $Name> extends SomeFailures ?
+																												ReportFailures<BaseChecks<v, Limits, $Name>> :
+																												_Parse<tail, Limits,
+																													Add<
+																														$<Strings.Length,v> extends 1 ? 'short' : 'long',
+																														$Name,
+																														v
+																													>
+																												>
+																										:
+																													
 
-	// Capture a short name & Done!
-  E extends `${infer v}`                           	? BaseChecks<v, Limits, $Name> extends SomeFailures ? ReportFailures<BaseChecks<v, Limits, $Name>> :
-																														Strings.Length<v> extends 1 ?
-																															$Name['short'] extends null ?
-																																_Parse<'', Limits, AddShort<$Name, v>> :
-																																_Parse<'', Limits, AddAliasShort<$Name, v>> :
-																															$Name['long'] extends null ?
-																																_Parse<'', Limits, AddLong<$Name, v>> :
-																																_Parse<'', Limits, AddAliasLong<$Name, v>> :
+  E extends `${infer v}`                           	? BaseChecks<v, Limits, $Name> extends SomeFailures ?
+																												ReportFailures<BaseChecks<v, Limits, $Name>> :
+																												_Parse<'', Limits,
+																													Add<
+																														$<Strings.Length,v> extends 1 ? 'short' : 'long',
+																														$Name,
+																														v
+																													>
+																												> 	
+																										:
 
 	Errors.Unknown
--- packages/@molt/types/tests/index.spec.ts
+++ packages/@molt/types/tests/index.spec.ts
@@ -65,14 +65,14 @@ namespace _case {
 
 //prettier-ignore
 namespace _mixed {
-	expectType<{ long: null; short: 'v'; aliases: { short: []; long: [] } }>(as<							Name.Parse<'-v'>>())
-	expectType<{ long: null; short: 'v'; aliases: { short: ['x']; long: [] } }>(as<						Name.Parse<'-v -x'>>())
-	expectType<{ long: null; short: 'v'; aliases: { short: ['x', 'y']; long: [] } }>(as<			Name.Parse<'-v -x -y'>>())
-	expectType<{ long: 'vv'; short: null; aliases: { short: []; long: ['xx'] } }>(as<					Name.Parse<'--vv --xx'>>())
-	expectType<{ long: 'vv'; short: null; aliases: { short: []; long: ['xx', 'yy'] } }>(as<		Name.Parse<'--vv --xx --yy'>>())
-	expectType<{ long: 'vv'; short: 'v'; aliases: { short: ['x']; long: ['xx'] } }>(as<				Name.Parse<'-v --vv -x --xx'>>())
-	expectType<{ long: 'vv'; short: 'v'; aliases: { short: ['x']; long: ['xx'] } }>(as<				Name.Parse<'v vv x xx'>>())
-	expectType<{ long: 'vv'; short: 'v'; aliases: { short: ['x']; long: ['xx'] } }>(as<				Name.Parse<'v --vv x xx'>>())
+	expectType<{ expression:string; canonical: 'v'; long: null; short: 'v'; aliases: { short: []; long: [] } }>(as<							Name.Parse<'-v'>>())
+	expectType<{ expression:string; canonical: 'v'; long: null; short: 'v'; aliases: { short: ['x']; long: [] } }>(as<						Name.Parse<'-v -x'>>())
+	expectType<{ expression:string; canonical: 'v'; long: null; short: 'v'; aliases: { short: ['x', 'y']; long: [] } }>(as<			Name.Parse<'-v -x -y'>>())
+	expectType<{ expression:string; canonical: 'vv'; long: 'vv'; short: null; aliases: { short: []; long: ['xx'] } }>(as<					Name.Parse<'--vv --xx'>>())
+	expectType<{ expression:string; canonical: 'vv'; long: 'vv'; short: null; aliases: { short: []; long: ['xx', 'yy'] } }>(as<		Name.Parse<'--vv --xx --yy'>>())
+	expectType<{ expression:string; canonical: 'vv'; long: 'vv'; short: 'v'; aliases: { short: ['x']; long: ['xx'] } }>(as<				Name.Parse<'-v --vv -x --xx'>>())
+	expectType<{ expression:string; canonical: 'vv'; long: 'vv'; short: 'v'; aliases: { short: ['x']; long: ['xx'] } }>(as<				Name.Parse<'v vv x xx'>>())
+	expectType<{ expression:string; canonical: 'vv'; long: 'vv'; short: 'v'; aliases: { short: ['x']; long: ['xx'] } }>(as<				Name.Parse<'v --vv x xx'>>())
 }
 
 interface SomeLong {
