75b45b80b3246ccc358d2fa5210f8bad80b6fab2
Jason Kuhrt <jasonkuhrt@me.com>
2023-11-18 08:12:03 -0500

    no exit in tests

--- packages/@molt/command/tests/_/helpers.ts
+++ packages/@molt/command/tests/_/helpers.ts
@@ -3,7 +3,7 @@ import { Command } from '../../src/index.js'
 import { isPromiseLikeValue } from '../../src/lib/prelude.js'
 import { z } from 'zod'
 
-export const $ = Command.create().use(Zod)
+export const $ = Command.create().use(Zod).settings({ onError: `throw` })
 
 export const assertAssignable = <T>(_: T): [T] => 0 as any // eslint-disable-line
 export const as = <T>(): T => undefined as any // eslint-disable-line
